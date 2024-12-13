cfb274a5b3c91cb624e0e7454c6e251dbfd6c90e
juan alex <juan_AVC789@hotmail.com>
2024-12-13 12:19:37 -0500

    ssss

--- app/Http/Controllers/print_controller.php
+++ app/Http/Controllers/print_controller.php
@@ -355,7 +355,7 @@ class print_controller extends Controller
 
         try {
             // Configuración de la impresora
-            $nombreImpresora = "cautivaprint";
+            $nombreImpresora = "cautivacode";
             $conector = new WindowsPrintConnector($nombreImpresora);
             $impresora = new Printer($conector);
             // Configurar el perfil de la impresora ADV-9010
