6723c40523a0c56d744cfa5345f61877b734b69b
juan alex <juan_AVC789@hotmail.com>
2024-12-17 12:15:24 -0500

    updated 16/12/2024

--- app/Http/Controllers/print_controller.php
+++ app/Http/Controllers/print_controller.php
@@ -403,10 +403,10 @@ class print_controller extends Controller
             'stretchtext' => 0  // Evitar la distorsión
         );
         // Generar código de barras con un tamaño adecuado
-        $pdf->write1DBarcode($request->input("barcode"), 'C128', '', '', '', 15, 10, $style, 'N'); 
+        $pdf->write1DBarcode($request->input("barcode"), 'C128', '', '', '', 8, 10, $style, 'N'); 
         $pdf->SetFont('helvetica', '', 7);
         $pdf->Cell(0, 1,$request->input("product_name"), 0, 1, 'C');
-        $pdf->SetFont('helvetica', '', 5);
+        $pdf->SetFont('helvetica', '', 7);
         $pdf->Cell(0, 1, $request->input("price"), 0, 1, 'C');
 
         // Ruta completa del archivo en la carpeta public
